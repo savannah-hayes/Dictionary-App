2880c286f7d63781789210e2c5f403e25a3cfc78
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-05-17 20:42:09 +0200

    Displayed audio player

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,12 +4,8 @@ import ReactAudioPlayer from "react-audio-player";
 function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <ReactAudioPlayer
-        src={props.phonetic.audio}
-        autoPlay={false}
-        controls={false}
-      />
       <p>{props.phonetic.text}</p>
+      <ReactAudioPlayer src={props.phonetic.audio} autoPlay={false} controls />
     </div>
   );
 }
